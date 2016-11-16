0acb63fbe7b9333a6484bb658767c62beb10db21
Rodrigo Alves <rodrigo.alves@cesar.org.br>
2016-11-16 12:49:27 -0300

    Add: Commands to be used by ioctl function

--- src/drivers/phy_driver_serial_linux.h
+++ src/drivers/phy_driver_serial_linux.h
@@ -6,3 +6,13 @@
  * of the BSD license. See the LICENSE file for details.
  *
  */
+enum serial_cmds {
+	SERIAL_CMD_PARENB,
+	SERIAL_CMD_CSTOPB,
+	SERIAL_CMD_CSIZE,
+	SERIAL_CMD_CS8,
+	SERIAL_CMD_CRTSCTS,
+	SERIAL_CMD_VIM,
+	SERIAL_CMD_VTIME,
+	SERIAL_CMD_BAUDRATE
+};
